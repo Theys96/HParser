46952d9373caf6ad9fb19c725907d8b597447ee2
Theys96 <accounts@thijshavinga.nl>
2018-05-07 23:56:10 +0200

    Clean up

--- grammar3.hs
+++
@@ -1,9 +0,0 @@
-import HParser.Grammar
-import HParser.Generator
-
-grammar = Grammar [
-   Rule (NonTerminal "S") [(NonTerminal "E"), (NonTerminal "S")],
-   Rule (NonTerminal "S") [],
-   Rule (NonTerminal "E") [(Terminal "e")],
-   Rule (NonTerminal "E") [(Terminal "k")]
-   ]
