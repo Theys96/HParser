8686148eab3ab4469712725e65c4716610cacd25
Theys96 <accounts@thijshavinga.nl>
2018-05-14 11:10:48 +0200

    Fixed grammar files

--- grammar4.hs
+++ grammar4.hs
@@ -3,5 +3,6 @@ module Grammar4 (grammar4) where
 import HParser.Grammar
 import HParser.Generator
 
-grammar4 = Grammar (NonTerminal "S") [
-   Rule 
+grammar4 = Grammar (NonTerminal "a") [
+   Rule (Terminal "a") [Terminal "aa"]
+]
--- grammar5.hs
+++ grammar5.hs
@@ -4,8 +4,8 @@ import HParser.Grammar
 import HParser.Generator
 
 grammar5 = Grammar (NonTerminal "S") [
-   Rule (NonTerminal "S") [(NonTerminal "E")],
-   Rule (NonTerminal "S") [(Terminal "a")],
-   Rule (NonTerminal "E") [(Terminal "a")],
-   Rule (NonTerminal "E") []
-   ]
+	Rule (NonTerminal "S") [NonTerminal "E"]
+	Rule (NonTerminal "S") [Terminal "a"]
+	Rule (NonTerminal "E") [Terminal "a"]
+	Rule (NonTerminal "E") []
+]
