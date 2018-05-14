35acd4c4a0c7d21acaa1311d1d051a3b8a7c2986
Theys96 <accounts@thijshavinga.nl>
2018-05-14 12:04:47 +0200

    Re-added Grammar4.hs

---
+++ Grammar4.hs
@@ -0,0 +1,10 @@
+module Grammar4 (grammar4) where
+
+import HParser.Grammar
+import HParser.Generator
+
+-- This is a corrupt grammar for testing
+grammar4 = Grammar (Terminal "a") [
+   Rule (Terminal "a") [Terminal "aa"]
+   ]
+
