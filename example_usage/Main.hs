c4baedaac18074247aaec2b419ae26e332167480
Theys96 <accounts@thijshavinga.nl>
2018-05-14 22:13:58 +0200

    Fixed example

--- example_usage/Main.hs
+++ example_usage/Main.hs
@@ -4,7 +4,8 @@ import Data.Tree
 main = do  
     putStrLn "Enter an expression:"  
     exp <- getLine  
-    putStr $ calcTree $ parseTree (toTokenList exp)
+    putStr $ show $ calcTree $ parseTree (toTokenList exp)
+    putStr "\n"
 
 toTokenList :: String -> [TokenTuple]
 toTokenList [] = []
@@ -26,9 +27,13 @@ toTokenList (c:list) = (charToken c, [c]):(toTokenList list)
       charToken '9' = NUM
 
 
-calcTree :: ParseTree -> Integer
-calcTree (Node (T NUM string) c) = read string :: Int
+calcTree :: ParseTree -> Int
+-- Sp can evaluate to epsilon. Currently this causes that S might have only 1 child.
+calcTree (Node (NT S) [a]) = (calcTree a)
+-- Other rules
 calcTree (Node (NT S) [a, b]) = (calcTree a) + (calcTree b)
 calcTree (Node (NT Sp) [plus, s]) = calcTree s
-calcTree (Node (NT E) [open, s, open]) = calcTree s
+calcTree (Node (NT Sp) []) = 0
+calcTree (Node (NT E) [open, s, close]) = calcTree s
 calcTree (Node (NT E) [num]) = calcTree num
+calcTree (Node (T NUM string) c) = read string :: Int
